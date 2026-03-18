02a5e5df3643e2c397218bef047ade970a70f952
HansKristofi <kristofihans@gmail.com>
2026-03-19 01:24:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/moszkvacafe-website/', // <-- Add your repository name here, surrounded by slashes
+  base: '/MoszkvaCafe2/', // <-- Add your repository name here, surrounded by slashes
 })
